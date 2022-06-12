9de17cf80b3a0c88452625a3f88b3576b72dfb27
Reuben Roshan <lelopopsinger@gmail.com>
2022-06-12 11:18:28 +0800

    Using React Context for global state

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,7 +1,39 @@
-import { Text } from 'react-native';
+import { useContext } from "react";
+import { Text, StyleSheet, View } from "react-native";
+import MealsList from "../components/MealsList/MealsList";
+import { FavouriteContext } from "../store/context/favorites-context";
+
+import { MEALS } from "../data/dummy-data";
 
 function FavoritesScreen() {
-  return <Text>The favorites screen!</Text>;
+	const favoriteMealsCtx = useContext(FavouriteContext);
+
+	const favoriteMeals = MEALS.filter(meals => {
+		return favoriteMealsCtx.ids.includes(meals.id);
+	});
+
+	if (favoriteMeals.length === 0) {
+		return (
+			<View style={styles.rootContainer}>
+				<Text style={styles.text}>You have no favourite meals yet</Text>
+			</View>
+		);
+	}
+
+	return <MealsList items={favoriteMeals} />;
 }
 
 export default FavoritesScreen;
+
+const styles = StyleSheet.create({
+	rootContainer: {
+		flex: 1,
+		justifyContent: "center",
+		alignItems: "center",
+	},
+	text: {
+		fontSize: 19,
+		color: "white",
+		fontWeight: "bold",
+	},
+});
